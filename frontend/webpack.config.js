cc0ae1a3bdb2aca98aad17a21de0c67c200a8b87
Sidorov Vitaly <vit_s71@yahoo.com>
2026-01-21 18:49:00 +0300

    webpack serv

---
+++ frontend/webpack.config.js
@@ -0,0 +1,37 @@
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from "copy-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: "./src/app.js",
+    mode: "development",
+    output: {
+        filename: "app.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+        compress: true,
+        port: 9000,
+        historyApiFallback: true, //для перевода на главную страницу при ошибке 404(если сервер не нашел страницу)
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html" // для копирования существующего файла, а не создания нового
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: "./src/templates", to: "templates"},
+                {from: "./src/static/images", to: "images"},
+            ],
+        }),
+    ],
+};
